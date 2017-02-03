f8f89972d685efe1abe897f12e3808827e4058d3
Dacian Spînu <macbook@Macs-MacBook-Air.local>
2017-02-03 15:09:35 +0200

    stuff

--- app/app.module.js
+++ app/app.module.js
@@ -4,6 +4,15 @@ angular.module('carlow.directives', []);
 
 var carlow = angular.module('carlow', ["ngFileUpload", "ui.router", "carlow.services", "carlow.controllers", "carlow.directives"]);
 
-carlow.config(function ($stateProvider, $urlRouterProvider, $httpProvider, $provide) {
-  console.log('carlow app angular');
+carlow.config(function ($stateProvider, $urlRouterProvider, $httpProvider, $provide, $locationProvider) {
+  $urlRouterProvider.when('', '/home');
+
+  var homeState = {
+    name: 'home',
+    url: '/home',
+    templateUrl: './app/views/mains/home.html'
+  };
+
+  $stateProvider
+    .state(homeState);
 });
