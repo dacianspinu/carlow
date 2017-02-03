8856e5f2ebe8dde24a452058bd43193ad9b2a305
Dacian Spînu <macbook@Macs-MacBook-Air.local>
2017-02-03 15:01:13 +0200

    home view

---
+++ app/views/mains/homeController.js
@@ -0,0 +1,25 @@
+(function () {
+  'use strict';
+  var controllerId = 'homeController';
+
+  var module = angular.module('carlow.controllers');
+
+  module.controller(controllerId,
+      ['$rootScope', '$scope', '$window', '$timeout', homeController]);
+
+  function homeController($rootScope, $scope, $window, $timeout) {
+    $(document).ready(function() {
+        $timeout(function() {
+            startInteractions();
+        }, 200);
+    })
+    
+    function startInteractions() {
+        $scope.blurredBackground = true;
+
+        $timeout(function() {
+            $scope.visibleUploadSection = true;
+        }, 300);
+    }
+  }
+})();
